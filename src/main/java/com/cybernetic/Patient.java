4db8737b98ff62098db34429af48d34970c023de
Malik <110349545+MalikAbdelrahim@users.noreply.github.com.>
2024-09-09 23:03:08 -0500

    Updating code

--- src/main/java/com/cybernetic/Patient.java
+++ src/main/java/com/cybernetic/Patient.java
@@ -6,11 +6,35 @@ public class Patient {
     private String name;
     private int age;
     private String medicalHistory;
-    private ArrayList installedOrgans = new ArrayList();
+    private String installedOrgans[] = new String[5];
+    private String CyberneticOrgan[]= new String[5];
+
 
     public String addOrgan(CyberneticOrgan organ)
     {
-            return "1";
+        String str = null;
+        int checker = 0;
+        for(int i =0; i< CyberneticOrgan.length; i++)
+        {
+            if(CyberneticOrgan[i] == null)
+                CyberneticOrgan[i] = organ.getModel();
+        }
+        for(int i =0; i< CyberneticOrgan.length; i++)
+        {
+            if(CyberneticOrgan[i].equalsIgnoreCase(organ.getModel()))
+                checker =1;
+        }
+        
+        if(checker == 1)
+        {
+            str = "Organ Added";
+            return str;
+        }
+        else if(checker == 0)
+        {
+           str ="Can not accept any more organs";
+           return str;
+        }
     }
 
     public String getPatientInfo()

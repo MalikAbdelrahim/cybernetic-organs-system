2b7fc149c6e0a748a921f8e34b348961fc281628
Malik <110349545+MalikAbdelrahim@users.noreply.github.com.>
2024-09-21 17:21:08 -0500

    test

---
+++ src/AlgorithmsandSorts.java
@@ -0,0 +1,14 @@
+public class AlgorithmsandSorts {
+
+    array[] list = new array[0];
+
+    public AlgorithmsandSorts(array[] Array)
+    {
+        list = array;
+    }
+
+    public void bubblesort()
+    {
+        
+    }
+}

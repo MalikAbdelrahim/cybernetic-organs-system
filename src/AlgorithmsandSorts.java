71b0c72043116490f5ff0b2978f0f58e87a4a8ad
Malik <110349545+MalikAbdelrahim@users.noreply.github.com.>
2024-09-21 17:29:11 -0500

    bubble sort complete

--- src/AlgorithmsandSorts.java
+++ src/AlgorithmsandSorts.java
@@ -9,7 +9,19 @@ public class AlgorithmsandSorts {
 
     public void bubblesort()
     {
+        for(int i=0; i<list.length -1; i++)
+            {
+                for(int j=0; i<list.length -i - 1; i++)
+                {
+                    if(array[j]>array[j+1])
+                    {
+                        int temp=array[j];
+                        array[j]=array[j+1];
+                        array[j+1]=temp;
 
+                    }
+                }
+            }
     }
 
     public void insertionSort()
@@ -34,6 +46,6 @@ public class AlgorithmsandSorts {
 
     public void binarySearch()
     {
-        
+
     }
 }

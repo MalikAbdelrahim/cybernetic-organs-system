147665d83ad5f62dc5dfe4a6072bd0e2fcffc017
Malik <110349545+MalikAbdelrahim@users.noreply.github.com.>
2024-09-24 09:02:40 -0500

    added mergesort and linearsearch

--- src/AlgorithmsandSorts.java
+++ src/AlgorithmsandSorts.java
@@ -1,13 +1,13 @@
+import java.lang.reflect.Array;
+
 public class AlgorithmsandSorts {
 
-    array[] list = new array[0];
-
-    public AlgorithmsandSorts(array[] Array)
+    public AlgorithmsandSorts()
     {
-        list = array;
+
     }
 
-    public void bubblesort()
+    public void bubblesort(int[] list)
     {
         for(int i=0; i<list.length -1; i++)
             {
@@ -24,14 +24,14 @@ public class AlgorithmsandSorts {
             }
     }
 
-    public void insertionSort()
+    public void insertionSort(int[] list)
     {
         for(int i =1; i <array.length; i++)
             {
                 int temp = list[i];
                 int j = i-1;
 
-                while(j>=0 && list[j] >temp)
+                while(j>=0 && list[j] > temp)
                 {
                     array[j+1]=array[j];
                     j--;
@@ -40,14 +40,14 @@ public class AlgorithmsandSorts {
             }
     }
 
-    public void selectionSort()
+    public void selectionSort(int[] list)
     {
         for(int i=0; i<list.length -1; i++)
         {
             int min=i;
-            for(int j=i+1; i<list.length; i++)
+            for(int j=i+1; j<list.length; j++)
             {
-                if(list[min]>list[j])
+                if(list[min] > list[j])
                 {
                     min =j;
                 }
@@ -60,14 +60,75 @@ public class AlgorithmsandSorts {
         }
     }
 
-    public void mergeSort()
+    public void mergeSort(int[] list)
     {
+        int length=list.length;
+        if(length<=1) return;
 
+        int middle = length/2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length - middle];
+        int i =0;
+        int j =0;
+        for(; i<length; i++)
+        {
+            if(i<middle)
+            {
+                leftArray[i]=list[i];
+            }
+            else{
+                rightArray[j]=list[i];
+                j++;
+            }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, list);
     }
 
-    public void linearSearch()
+    public void merge(int[] leftArray, int[]rightArray, int[] orgArray)
     {
+        int leftSize= orgArray.length/2;
+        int rightSize= orgArray.length-leftSize;
+        int i=0, l=0, r=0;
+        
+        while(l < leftSize && r < rightSize)
+        {
+            if(leftArray[l] < rightArray[r])
+            {
+                orgArray[i]=leftArray[l];
+                i++;
+                l++;
+            }
+            else 
+            {
+                orgArray[i] = rightArray[r];
+                i++;
+                r++;
+            }
+        }
+        while(l<leftSize)
+        {
+            orgArray[i]=leftArray[l];
+            i++;
+            l++;
+        }
+        while(r<rightSize)
+        {
+            orgArray[i]=rightArray[r];
+            i++;
+            r++;
+        }
+    }
 
+    public int linearSearch(int[] list, int value)
+    {
+        for(int i =0; i<array.length; i++)
+        {
+            if(array[i] == value) 
+            return i;
+        }
+        return -1;
     }
 
     public void binarySearch()
